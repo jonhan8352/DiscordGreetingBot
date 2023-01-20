eb84af5bf4c71aa5efd6c9d2e51fc3f4e5e32b9d
Jonathan Han <87901116+jonhan8352@users.noreply.github.com>
2023-01-20 13:26:05 +0800

    Update discordgreetingbot.py

--- discordgreetingbot.py
+++ discordgreetingbot.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def random_response(ctx):
     if ctx.channel.id == CHANNEL_ID:
-      responses = ["I'm doing well, thank you!", "I'm feeling great, thank you!", "I'm fine. How about you?", "I'm good. Anything I can do for you?", "I'm doing well. And you?]
+      responses = ["I'm doing well, thank you!", "I'm feeling great, thank you!", "I'm fine. How about you?", "I'm good. Anything I can do for you?", "I'm doing well. And you?"]
         await ctx.send(random.choice(responses))
     else:
         await ctx.send("This command can only be used in a specific channel.")
